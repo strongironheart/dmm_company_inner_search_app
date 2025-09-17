08ca379a985f272994bc7582a1f63c9cc5392c8b
Takuya.Y <tayamash@redhat.com>
2025-09-17 18:08:54 +0900

    fixed:20250917

--- components.py
+++ components.py
@@ -25,6 +25,7 @@ def display_select_mode():
     """
     回答モードのラジオボタンを表示
     """
+    st.markdown(f"#### {ct.PURPOSE}")
     # 回答モードを選択する用のラジオボタンを表示
     col1, col2 = st.columns([100, 1])
     with col1:
@@ -42,8 +43,13 @@ def display_initial_ai_message():
     """
     with st.chat_message("assistant"):
         # 「st.success()」とすると緑枠で表示される
-        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+#        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        st.success("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        st.warning("具体的に入力した方が期待通りの回答を得やすいです。",icon=ct.WARNING_ICON)
 
+    with st.sidebar:
+        #区切り線の表示
+        st.divider()
         # 「社内文書検索」の機能説明
         st.markdown("**【「社内文書検索」を選択した場合】**")
         # 「st.info()」を使うと青枠で表示される
@@ -86,12 +92,13 @@ def display_conversation_log():
 
                         # 参照元のありかに応じて、適したアイコンを取得
                         icon = utils.get_source_icon(message['content']['main_file_path'])
-                        # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
+                        # メインドキュメントのページ番号表示
                         if "main_page_number" in message["content"]:
+#                            st.success(f"{message['content']['main_file_path']}（{message['content']['main_page_number']}ページ）", icon=icon)
                             st.success(f"{message['content']['main_file_path']}", icon=icon)
                         else:
                             st.success(f"{message['content']['main_file_path']}", icon=icon)
-                        
+
                         # ==========================================
                         # ユーザー入力値と関連性が高いサブドキュメントのありかを表示
                         # ==========================================
@@ -103,7 +110,7 @@ def display_conversation_log():
                             for sub_choice in message["content"]["sub_choices"]:
                                 # 参照元のありかに応じて、適したアイコンを取得
                                 icon = utils.get_source_icon(sub_choice['source'])
-                                # 参照元ドキュメントのページ番号が取得できた場合にのみ、ページ番号を表示
+                                # サブドキュメントのページ番号表示
                                 if "page_number" in sub_choice:
                                     st.info(f"{sub_choice['source']}", icon=icon)
                                 else:
@@ -158,9 +165,9 @@ def display_search_llm_response(llm_response):
         # ページ番号が取得できた場合のみ、ページ番号を表示（ドキュメントによっては取得できない場合がある）
         if "page" in llm_response["context"][0].metadata:
             # ページ番号を取得
-            main_page_number = llm_response["context"][0].metadata["page"]
+            main_page_number = llm_response["context"][0].metadata["page"] + 1
             # 「メインドキュメントのファイルパス」と「ページ番号」を表示
-            st.success(f"{main_file_path}", icon=icon)
+            st.success(f"{main_file_path}（{main_page_number}ページ）", icon=icon)
         else:
             # 「メインドキュメントのファイルパス」を表示
             st.success(f"{main_file_path}", icon=icon)
@@ -193,7 +200,7 @@ def display_search_llm_response(llm_response):
             # ページ番号が取得できない場合のための分岐処理
             if "page" in document.metadata:
                 # ページ番号を取得
-                sub_page_number = document.metadata["page"]
+                sub_page_number = document.metadata["page"] + 1
                 # 「サブドキュメントのファイルパス」と「ページ番号」の辞書を作成
                 sub_choice = {"source": sub_file_path, "page_number": sub_page_number}
             else:
@@ -216,7 +223,7 @@ def display_search_llm_response(llm_response):
                 # ページ番号が取得できない場合のための分岐処理
                 if "page_number" in sub_choice:
                     # 「サブドキュメントのファイルパス」と「ページ番号」を表示
-                    st.info(f"{sub_choice['source']}", icon=icon)
+                    st.info(f"{sub_choice['source']}（{sub_choice['page_number']}ページ）", icon=icon)
                 else:
                     # 「サブドキュメントのファイルパス」を表示
                     st.info(f"{sub_choice['source']}", icon=icon)
--- main.py
+++ main.py
@@ -60,7 +60,8 @@ if not "initialized" in st.session_state:
 cn.display_app_title()
 
 # モード表示
-cn.display_select_mode()
+with st.sidebar:
+    cn.display_select_mode()
 
 # AIメッセージの初期表示
 cn.display_initial_ai_message()
